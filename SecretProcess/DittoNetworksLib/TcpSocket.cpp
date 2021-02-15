e443f1638e5dc15652f598b9536353b6fc3b275a
Tal Saadi <talsaadi36@gmail.com>
2021-02-15 15:10:42 +0200

    Add all tcp socket functionality

--- SecretProcess/DittoNetworksLib/TcpSocket.cpp
+++ SecretProcess/DittoNetworksLib/TcpSocket.cpp
@@ -10,12 +10,7 @@ TcpSocket::~TcpSocket()
 {
 	try
 	{
-		int result = closesocket(_socket);
-
-		if (result == SOCKET_ERROR)
-		{
-			throw WindowsException(WSAGetLastError());
-		}
+		close();
 	}
 	catch (const WindowsException& e)
 	{
@@ -25,6 +20,91 @@ TcpSocket::~TcpSocket()
 	{
 		OutputDebugStringW(L"Failed to close socket");
 	}
+}
+
+std::tuple<TcpSocket, std::wstring> TcpSocket::accept()
+{
+	return std::tuple<TcpSocket, std::wstring>();
+}
+
+void TcpSocket::bind(const std::string & address, const int port)
+{
+	sockaddr_in socket_address;
+	socket_address.sin_family = AF_INET;
+	socket_address.sin_addr.s_addr = inet_addr(address.c_str());
+	socket_address.sin_port = htons(port);
+
+	int result = ::bind(_socket, (SOCKADDR*)(&socket_address), sizeof(socket_address));
+
+	if (result == SOCKET_ERROR)
+	{
+		throw WindowsException(WSAGetLastError());
+	}
+}
+
+void TcpSocket::listen(const int connections)
+{
+	if (::listen(_socket, connections) == SOCKET_ERROR)
+	{
+		throw WindowsException(WSAGetLastError());
+	}
+}
+
+void TcpSocket::close()
+{
+	int result = closesocket(_socket);
+
+	if (result == SOCKET_ERROR)
+	{
+		throw WindowsException(WSAGetLastError());
+	}
+}
+
+void TcpSocket::connect(const std::string & address, const int port)
+{
+	sockaddr_in socket_address;
+	socket_address.sin_family = AF_INET;
+	socket_address.sin_addr.s_addr = inet_addr(address.c_str());
+	socket_address.sin_port = htons(port);
+
+	int result = ::connect(_socket, (SOCKADDR*)&socket_address, sizeof(socket_address));
+
+	if (result == SOCKET_ERROR) 
+	{
+		throw WindowsException(WSAGetLastError());
+	}
+}
+
+int TcpSocket::send(const std::string& data)
+{
+	int result = ::send(_socket, data.c_str(), data.size(), 0);
+
+	if (result == SOCKET_ERROR)
+	{
+		throw WindowsException(WSAGetLastError());
+	}
+
+	return result;
+}
+
+std::string TcpSocket::recv(const size_t length)
+{
+	char* recv_buffer = reinterpret_cast<char*>(malloc(length));
+
+	if (recv_buffer == nullptr)
+	{
+		throw WindowsException();
+	}
+
+	int result = ::recv(_socket, recv_buffer, length, 0);
+
+	if (result == SOCKET_ERROR)
+	{
+		free(recv_buffer);
+		throw WindowsException(WSAGetLastError());
+	}
+
+	return recv_buffer;
 }
 
 SOCKET TcpSocket::create_socket()
--- SecretProcess/DittoNetworksLib/TcpSocket.hpp
+++ SecretProcess/DittoNetworksLib/TcpSocket.hpp
@@ -3,6 +3,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
+#include <tuple>
 #include "WindowsException.hpp"
 
 const class TcpSocket
@@ -11,10 +12,18 @@ public:
 	explicit TcpSocket();
 	~TcpSocket();
 
+public:
+	std::tuple<TcpSocket, std::wstring> accept();
+	void bind(const std::string& address, const int port);
+	void listen(const int connections);
+	void close();
+	void connect(const std::string& address, const int port);
+	int send(const std::string& data);
+	std::string recv(const size_t length);
+
 private:
 	static SOCKET create_socket();
 
 private:
 	SOCKET _socket;
 };
-
